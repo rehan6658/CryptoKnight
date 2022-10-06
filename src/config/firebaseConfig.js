ebd21fd21749da7d8e15f67d3fb0066aef0e57bf
rehan6658 <sayyedrehanali9@gmail.com>
2022-10-06 07:55:28 +0530

    second commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAdIyLDneeaTpn3nNDpApBWuA0pYTzOPxw",
+  
+    authDomain: "cryptoknight-c0ab6.firebaseapp.com",
+  
+    projectId: "cryptoknight-c0ab6",
+  
+    storageBucket: "cryptoknight-c0ab6.appspot.com",
+  
+    messagingSenderId: "1073119769418",
+  
+    appId: "1:1073119769418:web:7e04c566f1f8c5e71a07b3"
+  
+  };
+  
+  
+  export default firebaseConfig;
